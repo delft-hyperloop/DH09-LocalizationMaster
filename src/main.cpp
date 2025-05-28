1affb00bd999425a2ef7de74d096f2ea70d594a4
JorianR <jrennen@delfthyperloop.nl>
2025-05-28 02:23:14 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,58 +1,161 @@
 #include <Arduino.h>
 #include <EEPROM.h>
-#define START_BYTE 0x80
-#define CMD_REQUEST_DATA 0x01
-#define CMD_SET_CONFIG   0x02
-#define EEPROM_ADDR_CONFIG 0
+
+#define RE_Sensor 2
+#define DE_Sensor 3
+
 #define RE_Senshub 19
 #define DE_Senshub 18
 
+#define STOP_BYTE 0x50
+#define START_BYTE 0x80
+#define CMD_REQUEST_DATA 0x01
+#define CMD_SET_CONFIG   0x02
+
+#define CONFIG_ADDR 0
 #define SLAVE_ID_ADDR 50 // Where we store slave ID
+
 uint8_t SLAVE_ID;
 uint8_t configValue;
-
+byte dataToSend[3];
 IntervalTimer timer;
 
-void sendResponse();
+int16_t velocityValue = 0;
+uint32_t positionValue = 0;
+uint8_t velocityArray[2];
+uint8_t positionArray[4];
+
+uint8_t frame[8];
+
+long trackData[2][2] = {{1188, 0}, {125856, 125516}};
+
+enum CommandType
+{
+  STOP,
+  CYCLIC_POS,
+  CYCLIC_POS_VEL,
+  SINGLE_POS,
+  SINGLE_POS_VEL,
+  STANDBY,
+  UNKNOWN,
+};
+
+void sendCommandToSensor(CommandType cmd);
+void sendData();
 
 void setup() {
+  // ---------------------- USB ---------------------
+  Serial.begin(9600); // USB serial
+  Serial.println("Booting up...");
+  // ------------------------------------------------
+
+  // ----------------- SENSOR INIT ----------------
+  Serial1.begin(115200); // Serial 1 is for receiving from the sensor
+  Serial6.begin(115200); // Serial 6 is for sending to the sensor
+  Serial1.setTimeout(1); // Set timeout for reading from RS-422
+  pinMode(DE_Sensor, OUTPUT); 
+  pinMode(RE_Sensor, OUTPUT);
+  Serial.println("Sensor initialized.");
+  // ----------------------------------------------
+
+  // --------------- SENSORHUB SETUP ---------------
+  Serial5.begin(115200); // Serial5 is for receiving from the sensor hub
+  Serial4.begin(115200); // Serial4 is for sending to the sensor hub
   pinMode(RE_Senshub, OUTPUT);
   pinMode(DE_Senshub, OUTPUT);
   digitalWrite(RE_Senshub, LOW); // Enable receiver mode (inverted)
   digitalWrite(DE_Senshub, HIGH); // Enable receiver mode (inverted)
-  Serial5.begin(115200); // Serial 5 is for receiving from the sensor hub
-  Serial4.begin(115200); // Serial4 is for sending to the sensor hub
-  Serial.begin(9600);
-  EEPROM.write(SLAVE_ID_ADDR, 0xB1); // Initialize EEPROM with size 512 bytes
+  Serial.println("Sensor hub initialized.");
+  // -----------------------------------------------
+  
+  // -------------------- EEPROM --------------------
+  // EEPROM.write(SLAVE_ID_ADDR, 0xB1); // Initialize EEPROM
   SLAVE_ID = EEPROM.read(SLAVE_ID_ADDR);
   if (SLAVE_ID == 0xFF) {
     SLAVE_ID = 0xB1; // Default ID if unset
     EEPROM.write(SLAVE_ID_ADDR, SLAVE_ID);
   }
-  configValue = EEPROM.read(EEPROM_ADDR_CONFIG);
+  configValue = EEPROM.read(CONFIG_ADDR);
+  Serial.println("EEPROM initialized.");
+  // ------------------------------------------------
+
+  // ----------------- SENSOR SETUP ----------------
+  sendCommandToSensor(STOP); // Request: stop sending
+  delay(1000);
+  sendCommandToSensor(CYCLIC_POS_VEL); // Request: send cyclic position and velocity
+  digitalWrite(DE_Sensor, LOW); // Disable driver mode
+  digitalWrite(RE_Sensor, LOW); // Enable receiver mode (inverted)
+
+  Serial.println("Sensor setup complete.");
+  // -----------------------------------------------
+
+  Serial.println("Setup complete.");
 }
 
 void loop() {
-  if (Serial5.available()>0) {
-    if (Serial5.peek() == START_BYTE) {
-      timer.begin(sendResponse, 1000); // Start the timer to send response every second
-      Serial.println(Serial5.read());
+  uint8_t received[9];
+
+  // TODO: 9 bytes is for cyclic, 7 for non cyclic. fix
+  if (Serial1.available() >= 9)
+  {
+    Serial1.readBytes(received, 9);
+
+    positionValue = 0;
+    velocityValue = 0;
+
+    // Get position from bytes
+    for (int i = 2; i < 6; i++)
+    {
+      positionValue = (positionValue << 8) | received[i];
+      positionArray[i-2] = received[i];
+    }
+
+    velocityArray[0] = received[6];
+    velocityArray[1] = received[7];
+
+    // Get velocity from bytes
+    for (int i = 6; i < 8; i++)
+    {
+      velocityValue = (velocityValue << 8) | received[i];
+    }
+    if (positionValue < 1188000){
+      Serial.print("\r||   Position: ");
+      Serial.print(positionValue);
+      Serial.print(". Velocity: ");
+      Serial.print(velocityValue);
+      Serial.print("   ||   ");
+      // Serial4.write(positionArray, 4); // Send data over RS-422
+      for (size_t i = 0; i < 4; i++)
+      {
+        frame[i+1] = positionArray[i];
+      }
+      frame[5] = (abs(velocityValue) >> 8);
+      frame[6] = (abs(velocityValue) & 0xFF);
+    }
+  }
+  if (Serial5.available() > 0) {
+    if (Serial5.read() == START_BYTE) {
+      timer.end(); // Stop the timer
+      timer.begin(sendData, 100000); // Start the timer to send response every 1/10second
+      Serial5.flush();
+    }
+    if (Serial5.read() == STOP_BYTE) {
+      timer.end(); // Stop the timer
       Serial5.flush();
     }
   }
 }
 
-void sendResponse() {
-  uint8_t frame[8];
+void sendData() {
   frame[0] = SLAVE_ID;
-  frame[1] = 0x01;
-  frame[2] = 0x02;
-  frame[3] = 0x03;
-  frame[4] = 0x04;
-  frame[5] = 0x05;
-  frame[6] = 0x06;
   frame[7] = (frame[0] + frame[1] + frame[2] + frame[3] + frame[4] + frame[5] + frame[6]) % 256;
-  Serial.println("Sending response: ");
+  for (size_t i = 0; i < 8; i++)
+  {
+    Serial.print("0x");
+    Serial.print(frame[i], HEX);
+    Serial.print(" ");
+  }
+  Serial.println();
   Serial4.write(frame, 8);
   Serial4.flush();
 }
@@ -72,17 +175,6 @@ void sendResponse() {
 // bool REQUEST_CYCLIC = true;
 
 // int msgLength = 9;
-// byte dataToSend[3];
-// enum CommandType
-// {
-//   STOP,
-//   CYCLIC_POS,
-//   CYCLIC_POS_VEL,
-//   SINGLE_POS,
-//   SINGLE_POS_VEL,
-//   STANDBY,
-//   UNKNOWN,
-// };
 
 // long lastSendTime = 0;
 // long requestInterval = 1000;
@@ -97,33 +189,13 @@ void sendResponse() {
 
 // bool sendToSensorhub = false;
 
-// uint32_t positionValue = 0;
-// uint8_t velocityArray[2];
-// uint8_t positionArray[4];
-// uint8_t sendArray[6];
 // uint32_t prevPosition = -1000;
-// int16_t vel = 0;
-
 // FlexCAN_T4<CAN3, RX_SIZE_256, TX_SIZE_16> can2;
 // CAN_message_t msg;
 
 // float precisionRangeStart = 20;
 // float precisionRangeEnd = 30;
 
-
-// void canSniff(const CAN_message_t &msg)
-// {
-//   Serial.print("     ||  CAN MESSAGES:  {");
-//   Serial.print("CAN ID: ");
-//   Serial.print(msg.id, HEX);
-//   Serial.print(" Buffer: ");
-//   for (uint8_t i = 0; i < msg.len; i++)
-//   {
-//     Serial.print(msg.buf[i]);
-//     Serial.print(" ");
-//   }
-//   Serial.print("}  ||");
-// }
 
 // void printByteArrayBinary(uint8_t *arr, size_t length)
 // {
@@ -139,113 +211,62 @@ void sendResponse() {
 //   }
 //   Serial.print(" }");
 // }
-// void sendCAN(int ID, uint8_t len, uint8_t *data)
-// {
-//   CAN_message_t msg;
-//   msg.id = ID; // Set the CAN ID
-//   msg.len = len; // Set the length of the message
-
-//   for (int i = 0; i < len; i++)
-//   {
-//     msg.buf[i] = data[i]; // Copy the received data to the CAN message buffer
-//   }
-
-//   can2.write(msg); // Send the CAN message
-// }
-// float decodeTemperature(uint8_t encoded) {
-//   if (encoded & 0x80) {  // High precision mode
-//       return precisionRangeStart + (encoded & 0x7F) / 10.0;
-//   } else {  // Integer mode
-//       return (float)encoded;
-//   }
-// }
 
 
-// CommandType getCommandFromString(const String &command)
-// {
-//   if (command == "STOP")
-//   {
-//     return STOP;
-//   }
-//   if (command == "CYCLIC_POS")
-//   {
-//     return CYCLIC_POS;
-//   }
-//   if (command == "CYCLIC_POS_VEL")
-//   {
-//     return CYCLIC_POS_VEL;
-//   }
-//   if (command == "SINGLE_POS")
-//   {
-//     return SINGLE_POS;
-//   }
-//   if (command == "SINGLE_POS_VEL")
-//   {
-//     return SINGLE_POS_VEL;
-//   }
-//   if (command == "STANDBY")
-//   {
-//     return STANDBY;
-//   }
-//   return UNKNOWN;
-// }
+void sendCommandToSensor(CommandType cmd)
+{
+  dataToSend[0] = 0xC0;
 
-// void sendCommandToSensor(const String command)
-// {
-//   dataToSend[0] = 0xC0;
+  switch (cmd)
+  {
+  case STOP:
+    dataToSend[1] = 0xF3;
+    dataToSend[2] = 0x33;
+    break;
+  case CYCLIC_POS:
+    dataToSend[1] = 0xF2;
+    dataToSend[2] = 0x32;
+    break;
+  case CYCLIC_POS_VEL:
+    dataToSend[1] = 0xF9;
+    dataToSend[2] = 0x39;
+    break;
+  case SINGLE_POS:
+    dataToSend[1] = 0xF1;
+    dataToSend[2] = 0x31;
+    break;
+  case SINGLE_POS_VEL:
+    dataToSend[1] = 0xF8;
+    dataToSend[2] = 0x38;
+    break;
+  case STANDBY:
+    dataToSend[1] = 0xFD;
+    dataToSend[2] = 0x3D;
+    break;
+  case UNKNOWN:
+    Serial.println("Unknown command");
+    return;
+    break;
+  default:
+    Serial.println("Unknown command");
+    return;
+    break;
+  }
 
-//   CommandType cmd = getCommandFromString(command);
+  Serial.print("Sending command: ");
+  Serial.println(cmd);
 
-//   switch (cmd)
-//   {
-//   case STOP:
-//     dataToSend[1] = 0xF3;
-//     dataToSend[2] = 0x33;
-//     break;
-//   case CYCLIC_POS:
-//     dataToSend[1] = 0xF2;
-//     dataToSend[2] = 0x32;
-//     break;
-//   case CYCLIC_POS_VEL:
-//     dataToSend[1] = 0xF9;
-//     dataToSend[2] = 0x39;
-//     break;
-//   case SINGLE_POS:
-//     dataToSend[1] = 0xF1;
-//     dataToSend[2] = 0x31;
-//     break;
-//   case SINGLE_POS_VEL:
-//     dataToSend[1] = 0xF8;
-//     dataToSend[2] = 0x38;
-//     break;
-//   case STANDBY:
-//     dataToSend[1] = 0xFD;
-//     dataToSend[2] = 0x3D;
-//     break;
-//   case UNKNOWN:
-//     Serial.println("Unknown command");
-//     return;
-//     break;
-//   default:
-//     Serial.println("Unknown command");
-//     return;
-//     break;
-//   }
+  digitalWrite(DE_Sensor, HIGH); // Enable driver mode
+  digitalWrite(RE_Sensor, HIGH); // Disable receiver mode (inverted)
 
-//   Serial.print("Sending command: ");
-//   Serial.println(command);
+  Serial6.write(dataToSend, sizeof(dataToSend)); // Send data over RS-422
+  Serial6.flush();                                // Wait for serial to finish sending
 
-//   digitalWrite(DE_Sensor, HIGH); // Enable driver mode
-//   digitalWrite(RE_Sensor, HIGH); // Disable receiver mode (inverted)
-//   // delay(5);  //allow sending to complete
-//   Serial6.write(dataToSend, sizeof(dataToSend)); // Send data over RS-422
-//   Serial6.flush();                                // Wait for serial to finish sending
+  Serial.println("Command sent!\n");
 
-//   Serial.println("Command sent!\n");
-
-//   digitalWrite(DE_Sensor, LOW); // Disable driver mode
-//   digitalWrite(RE_Sensor, LOW); // Enable receiver mode (inverted)
-// }
+  digitalWrite(DE_Sensor, LOW); // Disable driver mode
+  digitalWrite(RE_Sensor, LOW); // Enable receiver mode (inverted)
+}
 
 // void sendDataToSensorHub(uint32_t pos)
 // {
@@ -292,9 +313,6 @@ void sendResponse() {
 //   {
 //     sendCommandToSensor("CYCLIC_POS_VEL");
 //   }
-
-//   digitalWrite(DE_Sensor, LOW); // Disable driver mode
-//   digitalWrite(RE_Sensor, LOW); // Enable receiver mode (inverted)
 
 //   delay(50);
 
